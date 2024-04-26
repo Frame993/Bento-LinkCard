1f9bd3afb64c1ff26e4a01ef76c5e76c4aec67e5
Yordani <yalvarezb93@gmail.com>
2024-04-26 16:28:02 +0300

    Add data and styles

---
+++ src/interfaces/data.ts
@@ -0,0 +1,11 @@
+export interface DataJson {
+    description?:  string;
+    emphasis?:     string;
+    img_url?:      string;
+    description2?: string;
+    email?:        string;
+    tags?:         string[];
+    description3?: string;
+    emphasis2?:    string;
+    tag2?:         string[];
+}
